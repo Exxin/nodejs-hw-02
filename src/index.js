61695a49aa28914f00077c710add11f7da9ef66f
Exxin <exxinblood@gmail.com>
2024-06-17 11:27:36 +0300

    ф

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
-import express from "express";
-const index = express();
+// import express from 'express';
 
-index.listen(3000, () => console.log("Server running on 3000 PORT"));
+// const PORT = 1488;
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.json({
+//       message: 'Hello motherfucker!',
+//       description: '',
+//   });
+// });
+
+// app.use((req, res, next) => {
+//   console.log(`Time: ${new Date().toLocaleString()}`);
+//   next();
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+import { initMongoDB } from './db/initMongoDB.js';
+import { startServer } from './server.js';
+
+const bootstrap = async () => {
+  await initMongoDB();
+  startServer();
+};
+
+bootstrap();
